e835b1407574d50e39adda37959896af812a3ce5
Prosper Habada <prosperhabada@gmail.com>
2022-04-16 11:17:03 +0000

    resume update

--- src/components/menu.js
+++ src/components/menu.js
@@ -107,7 +107,7 @@ const Menu = ({ menuOpen, toggleMenu }) => {
                 </NavListItem>
               ))}
           </NavList>
-          <Link href="/" target="_blank" rel="nofollow noopener noreferrer">
+          <Link href="/resume.pdf" target="_blank" rel="nofollow noopener noreferrer">
             
           </Link>
         </NavLinks>
